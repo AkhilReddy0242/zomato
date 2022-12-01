5e260c410911ea3d78c126cd40f910ea67cc8ff1
Akhil Reddy <anreddyakhilreddy02@gmail.com>
2022-12-02 03:16:38 +0530

    Init the project

---
+++ src/components/Home/Homepage.js
@@ -0,0 +1,13 @@
+
+import Header from "./Header";
+import QuickSearch from "./QuickSearch";
+
+function HomePage() {
+  return (
+    <>
+      <Header />
+      <QuickSearch />
+    </>
+  );
+}
+export default HomePage;
